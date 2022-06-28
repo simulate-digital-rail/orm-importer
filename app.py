0c584ab6ac40541c678b209cc12168f095c3e9b9
Philipp Schmidt <philipp.schmidt@student.hpi.de>
2022-06-28 17:29:10 +0200

    create list of nodes and ways from ORM

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def run_converter():
     y2 = request.args.get('y2')
     if not x1 or not x2 or not y1 or not y2:
         return 'No location specified', 400
-    converter.run_converter(x1, y1, x2, y2)
+    return converter.run_converter(x1, y1, x2, y2), 200
--- converter.py
+++ converter.py
@@ -3,7 +3,7 @@ import overpy
 def build_query(x1,y1,x2,y2):
     x1,x2 = min(x1,x2), max(x1,x2)
     y1,y2 = min(y1,y2), max(y1,y2)
-    query = f'node["railway"]({x1},{y1},{x2},{y2});out;'
+    query = f'(way["railway"="rail"]({x1},{y1},{x2},{y2});node(w););out body;'
     print(query)
     return query
 
@@ -14,4 +14,23 @@ def query_orm(query):
 
 def run_converter(x1, y1, x2, y2):
     orm_objects = query_orm(build_query(x1,y1,x2,y2))
+    
+    result_str = ""
+
+    for node in orm_objects.nodes:
+        result_str += f"node {node.id} {node.lat} {node.lon} description\n"
+
+    for way in orm_objects.ways:
+        for idx, el in enumerate(way._node_ids):
+            if idx==0:
+                continue
+            
+            result_str += f"edge {way._node_ids[idx-1]} {el}\n"
+        first_node = way._node_ids[0]
+
+    return result_str
+        
+
+
+
     
