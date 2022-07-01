a4b6968fd482f162c0e327297db051536290a6d8
rohan.sawahn <rohan.sawahn@student.hpi.de>
2022-07-01 16:10:22 +0200

    added code to generate signals

--- converter.py
+++ converter.py
@@ -1,35 +1,59 @@
 from dataclasses import dataclass
 import overpy
+import numpy as np
 
-@dataclass
-class BoundingBox:
-    x1: float
-    y1: float
-    x2: float
-    y2: float
+from rail_types import BoundingBox, Signal
+api = overpy.Overpass()
 
-    def __repr__(self):
-        x1,x2 = min(self.x1,self.x2), max(self.x1,self.x2)
-        y1,y2 = min(self.y1,self.y2), max(self.y1,self.y2)
-        return f'{x1},{y1},{x2},{y2}'
+def get_all_rail_objects(bounding_box: BoundingBox):
+    query = f'(way["railway"]({bounding_box});node(w););out body;'
+    return query_old_api(query)
 
 def get_track_objects(bounding_box: BoundingBox):
     query = f'(way["railway"="rail"]({bounding_box});node(w););out body;'
-    return query_orm(query)
+    return query_old_api(query)
 
-def get_signal_objects(bounding_box: BoundingBox):
-    query = f'(node["railway"="signal"]({bounding_box}););out;'
-    return query_orm(query)
+def get_signals_objects(bounding_box: BoundingBox):
+    query = f'(way["railway"="signal"]({bounding_box});node(w););out body;'
+    return query_old_api(query)
 
-def query_orm(query):
-    api = overpy.Overpass()
+def query_old_api(query):
     result = api.query(query)
     return result
 
+def dist_nodes(n1, n2):
+    # Calculate distance between two nodes
+    p1 = (n1.lat, n1.lon)
+    p2 = (n2.lat, n2.lon)
+    return np.linalg.norm(p2-p1)
+
+def make_signal_string(signal, node_before, node_after):
+    distance_side = dist_side(node_before, signal)
+    distance_node_before = dist_nodes(node_before, signal)
+    kind = "andere"
+    function = "andere"
+    # ToDo extend arnes planpro generator to take dist_side as input, only then pos of signal is unambigous
+    signal_str = f"signal signal {node_before.id} {node_after.id} {distance_node_before} {function} {kind}]\n"
+    return signal_str
+
+
+def dist_side(node_before, node_after, signal):
+    # Calculate distance from point(signal) to edge between node before and after
+    p1 = (node_before.lat, node_before.lon)
+    p2 = (node_after.lat, node_after.lon)
+    p3 = (signal.lat, signal.lon)
+    return np.abs(np.cross(p2-p1, p1-p3)) / np.linalg.norm(p2-p1)
+
+def find_next_rail_node(index, way):
+    for i in range(index, len(way.nodes)):
+        if 'railway' in way.nodes[i].tags.keys() and way.nodes[i].tags['railway'] == 'rail':
+            return way.nodes[i]
+    raise Exception("No rail node after signal was found")
+
 def run_converter(x1, y1, x2, y2):
     bounding_box = BoundingBox(x1, y1, x2, y2)
+    all_objects = get_all_rail_objects(bounding_box)
     track_objects = get_track_objects(bounding_box)
-    signal_objects = get_signal_objects(bounding_box)
 
     result_str = ""
 
@@ -42,7 +66,18 @@ def run_converter(x1, y1, x2, y2):
                 continue
             
             result_str += f"edge {way._node_ids[idx-1]} {el}\n"
-        first_node = way._node_ids[0]
+
+    for way in all_objects.ways:
+        node_before = None
+        for idx, node in enumerate(way.nodes):
+            if 'railway' in node.tags.keys() and  node.tags['railway'] == 'rail':
+                node_before = node
+            if 'railway' in node.tags.keys() and  node.tags['railway'] == 'signal':
+                node_after = find_next_rail_node(idx, way)
+                if not node_before:
+                    raise Exception("No rail node before signal was found")
+                result_str += make_signal_string(signal=node, node_before=node_before,
+                                                 node_after=node_after)
 
     return result_str
         
